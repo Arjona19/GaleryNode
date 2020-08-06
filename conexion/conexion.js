544e18d79a417809618eddf5d2d622a992b5f75e
Santiago Arjona <wilberth2010@hotmail.com>
2020-08-05 22:36:04 -0500

    init1

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'galeria'
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b45edfa7a0e042',
+    password: '2d95def1',
+    database: 'heroku_dc6578ea22b1ea9'
 });
 
 connection.connect(
